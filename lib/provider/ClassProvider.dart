b9187f260bbef9e511ef9fbe5408ffaed5633d9e
haianh <haianhtranmanh@gmail.com>
2024-12-10 03:03:18 +0700

    fix merge

--- lib/provider/ClassProvider.dart
+++ lib/provider/ClassProvider.dart
@@ -87,8 +87,9 @@ class ClassProvider with ChangeNotifier {
         headers: {"Content-Type": "application/json"},
         body: json.encode(requestBody),
       );
+      print("hatest"+ response.body);
       String code = jsonDecode(response.body)['data'];
-      print(response.body);
+      print("test" + response.body);
       if (response.statusCode == 200) {
         Class newClass = Class.fromJson(json.decode(response.body)['data']);
         print(newClass);
